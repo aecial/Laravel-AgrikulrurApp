7f70b9f14e3b4d6e841949afb1b1102aefe9b0ba
CubeDenmark <cubdesdenmark06242gmail.com>
2023-10-02 12:05:16 +0800

    payment table

---
+++ database/migrations/2023_10_02_035404_create_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id('pay_id');
+            $table->integer('creator_id');
+            $table->integer('bidder_id');
+            $table->string('creator_status');
+            $table->string('bidder_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
